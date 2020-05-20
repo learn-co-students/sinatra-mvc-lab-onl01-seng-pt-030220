a79fe00c0fc04c6dc5793c082720f4a5379cdc70
Jade Lynn Masker <hopefulfire@gmail.com>
2020-05-20 15:59:53 -0400

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,17 +1,28 @@
 class PigLatinizer
 
-	def piglatinize(word)
-		if word.index(/[aeiou]/) == 0
-			word += 'w'
+	def initialize(text)
+		@original_text = text.downcase
+		@words = @original_text.split(' ')
+		pig_latinize
+		@pig_latin = @words.join(' ')
+	end
+
+	def pig_latinize
+		@words = @words.map do |word|
+			if word.index(/[aeiou]/) == 0
+				word += 'w'
+			end
+			while word.index(/[aeiou]/) != 0
+				character = word[0]
+				word = word[1..-1] + character
+			end
+			word += 'ay'
 		end
-		while word.index(/[aeiou]/) != 0
-			character = word[0]
-			word = word[1..-1] + character
-		end
-		word += 'ay'
 	end
 
 	def to_s
 		@pig_latin
 	end
 end
+
+puts PigLatinizer.new('Example')
